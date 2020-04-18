2f3be419c62c07e1eb4b74018086cbe3d146cf25
Anthony Ware <aware91coding@gmail.com>
2020-04-17 22:31:09 -0500

    added styles and have pictures aligin.

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -15,35 +15,56 @@ const CardBody = styled.div`
     display: flex;
     flex-flow: column wrap;
     justify-content: center;
-    align-items: center;
+    align-items: flex-start;
+    background: #EDF2B9;
 `;
 
 const CardTitle = styled.h2`
-    text-align: center;
+    display: flex;
+    flex-flow: row wrap;
+    justify-content: center;
+    background: #2DCF94;
+    width: 100%;
 `;
 
 const CardImg = styled.img`
     display: block;
-    margin-left: 7.5%;
-    margin-right: 5%;
-    width: 100%;
+    margin: 5%;
+    width: 90%;
 `;
 
 const CardText = styled.p`
     text-align: center;
+    margin: 3%;
 `;
 
+const Statues = styled.div`
+    display:flex;
+    flex-flow: row wrap;
+    justify-content: space-evenly;
+    align-items: space-evenly;
+    width: 100%;
+`;
+
+const StatuesText = styled.p`
+    width: 49%;
+`;
 const Character = ({ characters }) => {
     console.log('characters', characters)
     return (
             <Card>
+                <CardTitle>{characters.name}</CardTitle>
                 <CardBody>
-                    <CardTitle>{characters.name}</CardTitle>
                     <CardImg src={characters.image} alt={characters.name} />
-                    <CardText>{characters.status}</CardText>
-                    <CardText>{characters.species}</CardText>
-                    <CardText>{characters.type}</CardText>
-                    <CardText>{characters.gender}</CardText>
+                    <Statues>
+                        <StatuesText>Status: {characters.status}</StatuesText>
+                        <StatuesText>Species: {characters.species}</StatuesText>
+                        <StatuesText>Type: {characters.type}</StatuesText>
+                        <StatuesText>Gender: {characters.gender}</StatuesText>
+                    </Statues>
+                    <CardText>Origin: {characters.origin.name}</CardText>
+                    <CardText>Location: {characters.location.name}</CardText>
+                    <CardText>Created: {characters.created}</CardText>
                 </CardBody>
             </Card>
     );
